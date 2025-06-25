fb58c50764da5879539d967e5b9dbcae55630b6c
Nayan Suter <nayankbps@gmail.com>
2025-06-25 11:21:38 +0600

    first

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+'use client'
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <>
+      <h2>Nav component</h2>
+      <div className="px-6 md:px-16 lg:px-32">
+       
+       <h2>Header slider if</h2>
+       <h2>Home products here if</h2>
+        <h2>Featured component if</h2>
+        <h2>Banner component if</h2>
+       <h2>NewsLetest compo if</h2>
+       
+      </div>
+      <h2>Footer compo</h2>
+    </>
+  );
+};
+
+export default Home;
